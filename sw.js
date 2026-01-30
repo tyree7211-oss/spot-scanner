87b5976f084ccfb9cca0565b3c6293bacb61ee45
tyree7211-oss <tyree7211@gmail.com>
2026-01-30 14:17:26 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,14 @@
-const CACHE_NAME = "spot-scanner-cache-v1";
+const CACHE_NAME = "spot-scanner-v1";
 const ASSETS = ["./", "./index.html", "./manifest.json"];
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
 });
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
 });
